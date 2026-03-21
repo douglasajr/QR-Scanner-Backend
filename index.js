d23a348015dbad4c230371d76c3fb65859bc0559
douglasajr <Douglasmarcia0@gmail.com>
2026-03-21 15:22:56 -0600

    Ajuste de ruta y Creacion de endpoints de pases(crear, editar estado)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./src/routes/auth.js";
+import pasesRoutes from "./src/routes/pases.js";
 import cors from 'cors'
 dotenv.config();
 
@@ -11,10 +12,8 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 })
 
-app.use("/", authRoutes)
-app.use("/usuarios", authRoutes)
-
-
+app.use("/auth", authRoutes)
+app.use("/", pasesRoutes)
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
--- src/routes/pases.js
+++ src/routes/pases.js
@@ -1,9 +1,91 @@
 import express from "express"
-import { supabase } from "../supabase";
+import { supabase } from "../supabase.js";
 
 const router = express.Router()
 
 
-router.post("/pases", async (req, res) => {
-    const { }
+
+router.get("/pases", async (req, res) => {
+    const { data: pases, error } = await supabase
+        .from("pases")
+        .select("*")
+
+    if (error) {
+        console.log('error', error)
+        return res.status(500).json({ error: "Error al obtener pases" })
+    }
+    return res.status(200).json({ pases })
 })
+
+router.post("/pases", async (req, res) => {
+    const { tipo_pase_id, nombre_visitante, apellido_visitante, telefono, motivo, duracion, duracion_unidad, creado_por } = req.body
+
+    if (!nombre_visitante || !apellido_visitante || !tipo_pase_id || !telefono || !motivo || !duracion || !duracion_unidad || !creado_por) {
+        return res.status(400).json({ error: "Todos los campos son requeridos" })
+    }
+
+    const { data: nuevoPase, error } = await supabase
+        .from("pases")
+        .insert({
+            tipo_pase_id,
+            nombre_visitante,
+            apellido_visitante,
+            telefono,
+            motivo,
+            creado_por,
+            duracion,
+            duracion_unidad,
+        })
+        .select("*")
+        .single()
+
+    if (error) {
+        console.log('error', error)
+        return res.status(500).json({ error: "Error al crear pase" })
+    }
+    return res.status(201).json({ nuevoPase, message: "Pase creado exitosamente" })
+
+})
+
+router.get("/pases/:id", async (req, res) => {
+    const { id } = req.params
+
+    const { data: pase, error } = await supabase
+        .from("pases")
+        .select("*")
+        .eq("id", id)
+        .single()
+
+    if (error) {
+        console.log('error', error)
+        return res.status(500).json({ error: "Error al obtener pase" })
+    }
+    if (!pase) {
+        return res.status(404).json({ error: "Pase no encontrado" })
+    }
+    return res.status(200).json({ pase })
+})
+
+router.patch("/pases/:id", async (req, res) => {
+    const { id } = req.params
+    const { status } = req.body
+
+    const { data: paseActualizado, error } = await supabase
+        .from("pases")
+        .update({
+            status
+        })
+        .eq("id", id)
+        .select("*")
+        .single()
+
+    if (error) {
+        console.log('error', error)
+        return res.status(500).json({ error: "Error al actualizar pase" })
+    }
+    if (!paseActualizado) {
+        return res.status(404).json({ error: "Pase no encontrado" })
+    }
+    return res.status(200).json({ paseActualizado, message: "Pase actualizado exitosamente" })
+})
+export default router
